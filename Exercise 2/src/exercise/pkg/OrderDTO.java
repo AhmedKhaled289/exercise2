4b26284be7f71b9f10f1fe3e748a79c53a4d936c
AhmedKhaled289 <115522248+AhmedKhaled289@users.noreply.github.com>
2022-10-13 11:14:30 +0300

    Add files via upload

---
+++ Exercise 2/src/exercise/pkg/OrderDTO.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercise.pkg;
+
+/**
+ *
+ * @author moj04272
+ */
+public class OrderDTO {
+   private String name;
+     private String area;
+      private String item;
+       private int  quantity;
+       private String brand;
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the area
+     */
+    public String getArea() {
+        return area;
+    }
+
+    /**
+     * @param area the area to set
+     */
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    /**
+     * @return the item
+     */
+    public String getItem() {
+        return item;
+    }
+
+    /**
+     * @param item the item to set
+     */
+    public void setItem(String item) {
+        this.item = item;
+    }
+
+    /**
+     * @return the quantity
+     */
+    public int getQuantity() {
+        return quantity;
+    }
+
+    /**
+     * @param quantity the quantity to set
+     */
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    /**
+     * @return the brand
+     */
+    public String getBrand() {
+        return brand;
+    }
+
+    /**
+     * @param brand the brand to set
+     */
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+       
+       
+}
